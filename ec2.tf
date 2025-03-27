36b245bb78b598daba27f2c0c6ecfe931ddde89a
priyanka <piyach60@gmail.com>
2025-03-27 09:53:04 +0000

    added new keypair file

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,3 @@ resource "aws_instance" "my_instance" {
   }
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7hpjWg3O/9DKb2ilevwb/3uz0rS5whBzs2AcRJjRlk piyach60@gmail.com"
-  # public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFX8c1QL9PZTcDv0bP9q3Oer/Ui2X5wEBkbPNspr36K gaura@DESKTOP-69MDMK7"
-}
